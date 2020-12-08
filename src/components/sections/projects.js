547b3797d78990d4c33b0116b7ced355d8360c6c
Jason Nordheim <jason.nordheim@gmail.com>
2020-12-08 14:24:30 -0700

    created projects component

---
+++ src/components/sections/projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Projects = () => {
+    return (
+        <section id="projects" className="projects">
+            
+        </section>
+    )
+}
+
+export default Projects
