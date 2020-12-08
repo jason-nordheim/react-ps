f224b93fbd9963121bf9603ada447675fb80e738
Jason Nordheim <jason.nordheim@gmail.com>
2020-12-08 14:24:13 -0700

    created navbar component

---
+++ src/components/header/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href='#projects'>
+                        My
+                        work
+                    </a>
+                </li>
+                <li>
+                    <a href='#skills'>
+                        My
+                        skills
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        Hire
+                        me
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
