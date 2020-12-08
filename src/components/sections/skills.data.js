cd48ee53431fd0180ebff2274dbf8ba12a6d54c9
Jason Nordheim <jason.nordheim@gmail.com>
2020-12-08 14:24:58 -0700

    added data for skills

---
+++ src/components/sections/skills.data.js
@@ -0,0 +1,203 @@
+export const type = {
+    framework: 'framework', 
+    library: 'library', 
+    technology: 'technology', 
+    language: 'programming language', 
+    general: 'general', 
+}
+
+// const category = {
+//     technical: 'technical', 
+//     experiential: 'experiential', 
+// }
+
+// const stack = {
+//     ops: 'dev ops', 
+//     client: 'client-side', 
+//     server: 'server side', 
+//     db: 'database management', 
+//     full: 'full-stack', 
+// }
+
+export const skills = [
+    {
+        name: 'ActiveRecord',
+        type: type.framework, 
+    },
+    {
+        name: 'Ajax',
+        type: type.technology, 
+    },
+    {
+        name: 'C#',
+        type: type.language, 
+    },
+    {
+        name:'C++',
+        type: type.language, 
+    },
+    {
+        name: 'Express.js',
+        type: type.framework, 
+    },
+    {
+        name: "Firebase", 
+        type: type.technology, 
+    }, 
+    {
+        name: "Firestore", 
+        type: type.technology, 
+    }, 
+    {
+        name: 'Gatsby.js',
+        type: type.framework, 
+    },
+    {
+        name: 'Git',
+        type: type.technology,
+    },
+    {
+        name: 'Github',
+        type: type.technology, 
+    },
+    {
+        name: 'HTML/CSS',
+        type: type.language, 
+    },
+    {
+        name: 'Java',
+        type: type.language, 
+    },
+    {
+        name: 'JavaScript',
+        type: type.language, 
+    },
+    {
+        name: 'Jest',
+        type: type.library, 
+    },
+    {
+        name: 'JSON',
+        type: type.language
+    },
+    {
+        name: 'JWT',
+        type: type.technology
+    },
+    {
+        name: 'jQuery',
+        type: type.framework, 
+    },
+    {
+        name: 'Knex.js',
+        type: type.framework, 
+    },
+    {
+        name: 'Leaflet.js',
+        type: type.framework, 
+    },
+    {
+        name: 'Material UI',
+        type: type.framework, 
+    },
+    {
+        name: 'Markdown',
+        type: type.language
+    },
+    {
+        name:'MsSQL',
+        type: type.language
+    },
+    {
+        name: 'MongoDB',
+        type: type.language
+    },
+    {
+        name: '.NET Core/Framework',
+        type: type.framework, 
+    },
+    {
+        name: 'Node.js',
+        type: type.framework
+    },
+    {
+        name: 'Objection.js',
+        type: type.framework,
+    },
+    {
+        name: 'Oracle PL/SQL',
+        type: type.language
+    },
+    {
+        name: 'PHP',
+        type: type.language, 
+    },
+    {
+        name: 'PowerShell',
+        type: type.language
+    },
+    {
+        name: 'Python',
+        type: type.language,
+    },
+    {
+        name: 'PostgreSQL',
+        type: type.language, 
+    },
+    {
+        name: 'Project Management',
+        type: type.general, 
+    },
+    {
+        name: 'React',
+        type: type.framework, 
+    },
+    {
+        name: 'React Router',
+        type: type.library, 
+    },
+    {
+        name: 'Ruby',
+        type: type.language, 
+    },
+    {
+        name: 'Ruby on Rails',
+        type: type.language, 
+    },
+    {
+        name: 'SASS/SCSS',
+        type: type.framework, 
+    },
+    {
+        name: 'Styled Components',
+        type: type.library
+    },
+    {
+        name: 'SQL',
+        type: type.language
+    },
+    {
+        name: 'Time Management', 
+        type: type.general
+    }, 
+    {
+        name: 'T-SQL',
+        type: type.language, 
+    },
+    {
+        name: 'TypeScript',
+        type: type.language, 
+    },
+    {
+        name: 'Visual Basic (VBA/VBS)',
+        type: type.language, 
+    },
+    {
+        name: 'Vue.js',
+        type: type.framework, 
+    },
+    {
+        name: 'Wordpress',
+        type: type.technology, 
+    }
+];
