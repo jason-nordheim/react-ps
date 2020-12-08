643794d7b0b8f96323e3f5fc5afd17c76bc0b716
Jason Nordheim <jason.nordheim@gmail.com>
2020-12-08 14:23:27 -0700

    added stylesheet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App/App";
+import App from "./components/app";
+import './styles/dist/main.css'
 
 ReactDOM.render(
   <React.StrictMode>
